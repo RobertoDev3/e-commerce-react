d975014e8d33ed51f2b9ce0c1e894f7157a47214
robertodev3 <roberto.dev3@hotmail.com>
2024-03-05 16:03:23 -0300

    ⚡️ Ajustando Mobile

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,10 +10,12 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      <div className="nav-logo">
-        <img src={logo} alt="" />
-        <p>SHOPPER</p>
-      </div>
+      <Link className="link" to="/">
+        <div className="nav-logo">
+          <img src={logo} alt="" />
+          <p>SHOPPER</p>
+        </div>
+      </Link>
       <ul className="nav-menu">
         <Link className="link" to="/">
           Shop
