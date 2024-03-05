2b1487bde28dd0ab4d4b18c8b5ed6d87933da40c
robertodev3 <roberto.dev3@hotmail.com>
2024-03-05 00:21:35 -0300

    ✨ Caixa de descrição do produto

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,28 @@
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="description-box">
+      <div className="description-box-navgator">
+        <div className="description-box-nav-box">Description</div>
+        <div className="description-box-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="description-box-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, molestiae
+          recusandae. Sit culpa id rem natus, maiores commodi voluptas neque.
+          Eum quia quam numquam earum at quisquam, vitae voluptatibus
+          dignissimos.
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            ducimus excepturi amet error animi, id quasi aliquid. Quisquam
+            repellendus inventore totam vel deleniti quas delectus, sunt sit.
+            At, omnis necessitatibus!
+          </p>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
