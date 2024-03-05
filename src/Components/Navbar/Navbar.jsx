3bbe057a981fe57b88b82993599008cb6b2d6239
robertodev3 <roberto.dev3@hotmail.com>
2024-03-05 01:47:12 -0300

    ✨ Carrinho de comprar adicionado

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
 import logo from "../../Assets//logo.png";
 import cart_icon from "../../Assets/cart_icon.png";
 import { Link } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
 
 const Navbar = () => {
+  const { getTotalCartItems } = useContext(ShopContext);
+
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -32,7 +35,7 @@ const Navbar = () => {
         <Link to="/cart">
           <img src={cart_icon} alt="" />
         </Link>
-        <div className="nav-cart-count">0</div>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
   );
--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,9 +1,13 @@
 import "./ProductDisplay.css";
 import star_icon from "../../Assets/star_icon.png";
 import star_dull_icon from "../../Assets/star_dull_icon.png";
+import { useContext } from "react";
+import { ShopContext } from "../../Context/ShopContext";
 
 const ProductDisplay = (props) => {
   const { product } = props;
+  const { addToCart } = useContext(ShopContext);
+
   return (
     <div className="product-display">
       <div className="product-display-left">
@@ -57,7 +61,13 @@ const ProductDisplay = (props) => {
             <div>XXL</div>
           </div>
         </div>
-        <button>ADD TO CART</button>
+        <button
+          onClick={() => {
+            addToCart(product.id);
+          }}
+        >
+          ADD TO CART
+        </button>
         <p className="product-display-right-category">
           <span>Category: </span>Women, T-Shirt, Crop Top
         </p>
